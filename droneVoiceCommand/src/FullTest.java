780956e3182bacfa6a6ebb6937c5a33e4f7f20f9
Hugo Roussel <hugo.rous@gmail.com>
2018-11-23 18:48:19 +0100

    Add nice summary result for kppv in fullTest

--- droneVoiceCommand/src/FullTest.java
+++ droneVoiceCommand/src/FullTest.java
@@ -17,25 +17,30 @@ public class FullTest {
 		Dataset datasetRef = new Dataset(ref, true);
 		Dataset datasetExp = new Dataset(hyp, false);
 		final int KMIN = 1;
-		final int KMAX = 6;
-		//double[][] summary = new double[4][KMAX-KMIN];
+		final int KMAX = 8;
+		ResultObject[][] summary = new ResultObject[4][KMAX-KMIN];
+		ResultObject ro;
 		
 		for(int k = KMIN; k < KMAX; ++k){
-			System.out.println("\nKPPV with ACP, k = "+k);
+			System.out.println("\nKPPV with ACP, k descend, k = "+k);
 			myKPPV kppvAcp = new myKPPV();
 			String[] colorLabel=kppvAcp.kppv(datasetRef, datasetExp, k);
 			for(int i=0; i<colorLabel.length; i++) {
 				System.out.println(datasetExp.records.get(i).path + " have the label : " + colorLabel[i]);
 			}
-			ACPTest.confusionMatrix(datasetRef, datasetExp, colorLabel);
+			ro = new ResultObject(datasetRef, datasetExp, colorLabel);
+			myDTWtest.printConfusion(ro.getConfusion());
+			summary[0][k-KMIN] = ro;
 			
-			System.out.println("\nKPPV without ACP, k = "+k);
+			System.out.println("\nKPPV without ACP, k descend, k = "+k);
 			myKPPV kppvNoAcp = new myKPPV(false, false);
 			colorLabel=kppvNoAcp.kppv(datasetRef, datasetExp, k);
 			for(int i=0; i<colorLabel.length; i++) {
 				System.out.println(datasetExp.records.get(i).path + " have the label : " + colorLabel[i]);
 			}
-			ACPTest.confusionMatrix(datasetRef, datasetExp, colorLabel);
+			ro = new ResultObject(datasetRef, datasetExp, colorLabel);
+			myDTWtest.printConfusion(ro.getConfusion());
+			summary[1][k-KMIN] = ro;
 			
 			System.out.println("\nKPPV with ACP, k augmente, k = "+k);
 			myKPPV kppvAcpA = new myKPPV(true,true);
@@ -43,7 +48,9 @@ public class FullTest {
 			for(int i=0; i<colorLabel.length; i++) {
 				System.out.println(datasetExp.records.get(i).path + " have the label : " + colorLabel[i]);
 			}
-			ACPTest.confusionMatrix(datasetRef, datasetExp, colorLabel);
+			ro = new ResultObject(datasetRef, datasetExp, colorLabel);
+			myDTWtest.printConfusion(ro.getConfusion());
+			summary[2][k-KMIN] = ro;
 			
 			System.out.println("\nKPPV without ACP, k augmente, k = "+k);
 			myKPPV kppvNoAcpA = new myKPPV(false, true);
@@ -51,7 +58,15 @@ public class FullTest {
 			for(int i=0; i<colorLabel.length; i++) {
 				System.out.println(datasetExp.records.get(i).path + " have the label : " + colorLabel[i]);
 			}
-			ACPTest.confusionMatrix(datasetRef, datasetExp, colorLabel);
+			ro = new ResultObject(datasetRef, datasetExp, colorLabel);
+			myDTWtest.printConfusion(ro.getConfusion());
+			summary[3][k-KMIN] = ro;
+		}
+		
+		System.out.println("\nSummary KPPV failure rate :");
+		System.out.println("   ACP k descend \t noACP k descend \t ACP k augment \t\t noACP k augment");
+		for(int j = 0; j < KMAX-KMIN; ++j){
+			System.out.println("k="+(j+KMIN)+" "+summary[0][j].getFailureRate()+"\t"+summary[1][j].getFailureRate()+"\t"+summary[2][j].getFailureRate()+"\t"+summary[3][j].getFailureRate());
 		}
 		
 		System.out.println("\nDTW");
