355c4c39f6981e6092c9b9aed9b29aa38eb8755a
Hugo Roussel <hugo.rous@gmail.com>
2018-11-23 20:01:21 +0100

    fix some problem in tests file and limit k to the number of record

--- droneVoiceCommand/src/FullTest.java
+++ droneVoiceCommand/src/FullTest.java
@@ -5,8 +5,8 @@ import fr.enseeiht.danck.voice_analyzer.defaults.DTWHelperDefault;
 public class FullTest {
 
 	public static void main(String[] args) {
-		String ref = "Tests/test_1/ref";
-		String hyp = "Tests/test_1/hyp";
+		String ref = "Tests/test_4/ref";
+		String hyp = "Tests/test_4/hyp";
 		
 		fullTest(ref, hyp);
 
@@ -21,7 +21,7 @@ public class FullTest {
 		ResultObject[][] summary = new ResultObject[4][KMAX-KMIN];
 		ResultObject ro;
 		
-		for(int k = KMIN; k < KMAX; ++k){
+		for(int k = KMIN; k < KMAX && k < datasetRef.records.size(); ++k){
 			System.out.println("\nKPPV with ACP, k descend, k = "+k);
 			myKPPV kppvAcp = new myKPPV();
 			String[] colorLabel=kppvAcp.kppv(datasetRef, datasetExp, k);
@@ -65,7 +65,7 @@ public class FullTest {
 		
 		System.out.println("\nSummary KPPV failure rate :");
 		System.out.println("   ACP k descend \t noACP k descend \t ACP k augment \t\t noACP k augment");
-		for(int j = 0; j < KMAX-KMIN; ++j){
+		for(int j = 0; j < KMAX-KMIN && j < (datasetRef.records.size()-KMIN); ++j){
 			System.out.println("k="+(j+KMIN)+" "+summary[0][j].getFailureRate()+"\t"+summary[1][j].getFailureRate()+"\t"+summary[2][j].getFailureRate()+"\t"+summary[3][j].getFailureRate());
 		}
 		
